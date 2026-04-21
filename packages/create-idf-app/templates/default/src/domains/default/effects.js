4bb97ad2da131c9ae4361420a40c3c6a987354db
ignatdubovskiy <dubovskiy.im@gmail.com>
2026-04-21 14:35:04 +0300

    feat(create-idf-app): template hello-world Task-домен

---
+++ packages/create-idf-app/templates/default/src/domains/default/effects.js
@@ -0,0 +1,7 @@
+// Кастомные effects для intent'ов, которые не покрываются generic HTTP-runner'ом.
+// Пример:
+// export const effects = {
+//   myCustomIntent: ({ intent, world }) => [ /* effects */ ],
+// };
+
+export const effects = {};
